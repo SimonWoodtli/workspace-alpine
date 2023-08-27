6546eb955d74306e40e781b9ea917b5945b41b4e
SimonWoodtli <xnasero@posteo.net>
2023-08-27 14:27:22 -0500

    🔧 add Container post-install script

---
+++ post-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+## manage host from within workspace
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
