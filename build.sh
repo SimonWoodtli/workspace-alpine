9b814b93d8845e5d78561188c835e86acffcb068
SimonWoodtli <xnasero@posteo.net>
2023-08-31 17:33:00 -0500

    🔧 bash default shell symlink to /usr

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -uex pipefail
-
-apk update && apk upgrade
+apk update && apk upgrade --no-cache
 
 for apkPackage in $(yq '.apk[]' < /tmp/packages.yml); do
   apk add --update --no-cache "$apkPackage"
 done
-
 for cpanPackage in $(yq '.cpan[]' < /tmp/packages.yml); do
   cpan -I "$cpanPackage"
 done
-
 curl -LJ https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o /tmp/uplink_linux_amd64.zip
 unzip /tmp/uplink_linux_amd64.zip -d /usr/local/bin/
 chmod 755 /usr/local/bin/uplink
-#while read -r line; do
-#  apk add --update --no-cache $(yq '.apk[]')
-#done <packages.yml
+# make bash default shell for root and symlink to /usr
+usermod --shell /bin/bash root
+ln -s /bin/bash /usr/bin/bash
+ln -s /bin/cat /usr/bin/cat
